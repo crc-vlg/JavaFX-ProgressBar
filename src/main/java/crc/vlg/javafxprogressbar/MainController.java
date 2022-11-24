a01fa280dacfce1f20fc9638f509ad38bb898553
crc-vlg <crc.vlg@gmail.com>
2022-11-24 21:48:28 +0300

    создание проекта

---
+++ src/main/java/crc/vlg/javafxprogressbar/MainController.java
@@ -0,0 +1,15 @@
+package crc.vlg.javafxprogressbar;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
+public class MainController {
+    @FXML
+    private Label label1;
+
+    @FXML
+    protected void onButtonClick() {
+
+    }
+}
